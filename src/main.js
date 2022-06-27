423278831336212bae1f448f15634c8b02b7f137
CruzCortesGonzalo <89588323+CruzCortesGonzalo@users.noreply.github.com>
2022-06-27 04:15:42 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/8-jSK1g4leeXwv-T/scene.splinecode');
